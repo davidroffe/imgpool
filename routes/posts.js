8f3c5f79caa9a7431ce3e657177274167b306743
David Roffe <davidroffe@gmail.com>
2019-08-26 16:37:25 -0700

    Successefully storing post information into database

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,19 @@
 const path = require('path');
 const rootPath = path.dirname(require.main.filename);
 const multer = require('@koa/multer');
-const upload = multer({ dest: rootPath + '/uploads' });
+const sizeOf = require('image-size');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, rootPath + '/uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1]
+    );
+  }
+});
+const upload = multer({ storage: storage });
 
 module.exports = (Models, router) => {
   router.get('/post/get', async ctx => {
@@ -10,10 +22,17 @@ module.exports = (Models, router) => {
     ctx.body = allPosts;
   });
   router.post('/post/create', upload.single('image'), async ctx => {
-    const allPosts = await Models.Post.findAll();
+    const dimensions = sizeOf(ctx.file.path);
     const tags = ctx.query.tags;
     const source = ctx.query.source;
-    ctx.body = allPosts;
+    const newPost = await Models.Post.create({
+      height: dimensions.height,
+      width: dimensions.width,
+      source: source,
+      url: '/uploads/' + ctx.file.filename
+    });
+
+    ctx.body = newPost;
   });
   router.post('/posts/delete', async ctx => {
     const allPosts = await Models.Post.findAll();
