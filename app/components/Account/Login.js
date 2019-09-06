5f41512758b5e9528667e0e22f8ff7c810e390a3
David Roffe <davidroffe@gmail.com>
2019-09-06 09:17:09 -0700

    Converted Account/Login to a functional component

--- app/components/Account/Login.js
+++ app/components/Account/Login.js
@@ -1,86 +1,79 @@
 import React from 'react';
 import Input from '../Utility/Input';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = props.handleChange;
-    this.handleSubmit = props.handleSubmit;
-    this.handleSwitchForm = props.handleSwitchForm;
-  }
-  render() {
-    return (
-      <div id="account-center">
-        <div id="center-box">
-          <form className="form-dark" onSubmit={this.handleSubmit}>
+const Login = props => {
+  return (
+    <div id="account-center">
+      <div id="center-box">
+        <form className="form-dark" onSubmit={props.handleSubmit}>
+          <Input
+            id="email"
+            autoComplete={'off'}
+            type={'text'}
+            title={'Full Name'}
+            name={'email'}
+            value={props.email}
+            placeholder={'EMAIL'}
+            handleChange={props.handleChange}
+          />
+          {props.form === 'signUp' ? (
             <Input
-              id="email"
+              id="username"
               autoComplete={'off'}
               type={'text'}
-              title={'Full Name'}
-              name={'email'}
-              value={this.props.email}
-              placeholder={'EMAIL'}
-              handleChange={this.handleChange}
+              title={'Username'}
+              name={'username'}
+              value={props.username}
+              placeholder={'USERNAME'}
+              handleChange={props.handleChange}
             />
-            {this.props.form === 'signUp' ? (
-              <Input
-                id="username"
-                autoComplete={'off'}
-                type={'text'}
-                title={'Username'}
-                name={'username'}
-                value={this.props.username}
-                placeholder={'USERNAME'}
-                handleChange={this.handleChange}
-              />
-            ) : null}
+          ) : null}
+          <Input
+            id="password"
+            autoComplete={'off'}
+            type={'password'}
+            title={'Password'}
+            name={'password'}
+            value={props.password}
+            placeholder={'PASSWORD'}
+            handleChange={props.handleChange}
+          />
+          {props.form === 'signUp' ? (
             <Input
-              id="password"
+              id="passwordConfirm"
               autoComplete={'off'}
               type={'password'}
-              title={'Password'}
-              name={'password'}
-              value={this.props.password}
-              placeholder={'PASSWORD'}
-              handleChange={this.handleChange}
+              title={'password-confirm'}
+              name={'password-confirm'}
+              value={props.passwordConfirm}
+              placeholder={'CONFIRM PASSWORD'}
+              handleChange={props.handleChange}
             />
-            {this.props.form === 'signUp' ? (
-              <Input
-                id="passwordConfirm"
-                autoComplete={'off'}
-                type={'password'}
-                title={'password-confirm'}
-                name={'password-confirm'}
-                value={this.props.passwordConfirm}
-                placeholder={'CONFIRM PASSWORD'}
-                handleChange={this.handleChange}
-              />
-            ) : null}
-            {this.props.errorMessage.map((errorMessage, index) => {
+          ) : null}
+          <div className="error-messages">
+            {props.errorMessage.map((errorMessage, index) => {
               return (
                 <p key={index} className="error">
                   {errorMessage}
                 </p>
               );
             })}
-            <Input
-              className="border-button"
-              type={'submit'}
-              value={this.props.form === 'login' ? 'LOGIN' : 'SIGN UP'}
-            />
-            <p>
-              Don‘t have an account?{' '}
-              <button className="switch-form" onClick={this.handleSwitchForm}>
-                {this.props.form === 'login' ? 'Sign Up' : 'Login'}
-              </button>
-            </p>
-          </form>
-        </div>
+          </div>
+          <Input
+            className="border-button"
+            type={'submit'}
+            value={props.form === 'login' ? 'LOGIN' : 'SIGN UP'}
+          />
+          <p>
+            Don‘t have an account?{' '}
+            <button className="switch-form" onClick={props.handleSwitchForm}>
+              {props.form === 'login' ? 'Sign Up' : 'Login'}
+            </button>
+          </p>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
