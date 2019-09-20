1105e667762fcc58d8afa37b545cb91e547bd653
David Roffe <davidroffe@gmail.com>
2019-09-19 19:44:11 -0700

    Set server to listen to port set in .env file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Router = require('@koa/router');
 const logger = require('koa-logger');
 const routes = require('./routes');
 const validate = require('./utility/validate');
+require('dotenv').config();
 const app = new Koa();
 const apiRouter = new Router({
   prefix: '/api'
@@ -29,5 +30,5 @@ app.use(validate);
 routes(apiRouter);
 app.use(apiRouter.routes());
 app.use(apiRouter.allowedMethods());
-const server = app.listen(3000);
+const server = app.listen(process.env.PORT);
 module.exports = server;
