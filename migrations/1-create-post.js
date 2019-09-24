ddd05f55c492aba7724db7aef9a925f169bee1ad
David Roffe <davidroffe@gmail.com>
2019-09-24 13:42:20 -0700

    Prefixed sequelize migration files with numbers for ordered sequence

---
+++ migrations/1-create-post.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      height: {
+        type: Sequelize.INTEGER
+      },
+      width: {
+        type: Sequelize.INTEGER
+      },
+      source: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      thumbUrl: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('Posts');
+  }
+};
