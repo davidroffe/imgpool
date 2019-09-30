7b319a230404efd2a15d44509497418b365bfe48
David Roffe <davidroffe@gmail.com>
2019-09-29 17:12:17 -0700

    API endpoint for updating tag state

--- routes/tag.js
+++ routes/tag.js
@@ -9,9 +9,31 @@ module.exports = (Models, router) => {
 
     ctx.body = allTags;
   });
-  router.post('/tag/delete', async ctx => {
-    const allTags = await Models.Tag.findAll();
 
-    ctx.body = allTags;
+  router.post('/tag/toggle', async ctx => {
+    const sessionId = ctx.cookies.get('auth');
+    const tagIds = ctx.query['tagIds[]'];
+
+    if (sessionId) {
+      const user = await Models.User.findOne({
+        where: { sessionId: sessionId }
+      });
+
+      if (user.admin) {
+        const tags = await Models.Tag.findAll({
+          where: { id: tagIds }
+        });
+        for (var i = 0; i < tags.length; i++) {
+          tags[i].active = !tags[i].active;
+          tags[i].save();
+        }
+
+        ctx.body = tags;
+      } else {
+        ctx.throw(401, 'Unauthorized request.');
+      }
+    } else {
+      ctx.throw(401, 'Unauthorized request.');
+    }
   });
 };
