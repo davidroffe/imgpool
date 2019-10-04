e0c6b5946c6f6ecd52904eb7b03c25397247b3e2
David Roffe <davidroffe@gmail.com>
2019-10-03 19:32:11 -0700

    Updated Modal component to include hide/show functionality from within

--- app/components/Utility/Modal.js
+++ app/components/Utility/Modal.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Modal = props => {
-  return (
+  return props.show ? (
     <div id="modal-container" onClick={props.toggleModal}>
       <div id="modal" onClick={e => e.stopPropagation()}>
         {props.children}
       </div>
     </div>
-  );
+  ) : null;
 };
 
 Modal.propTypes = {
   toggleModal: PropTypes.func.isRequired,
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
+  show: PropTypes.bool.isRequired
 };
 
 export default Modal;
