adbb5cfad51160a7e2df8f63a8ad7e46c67c4ba6
David Roffe <davidroffe@gmail.com>
2019-10-04 18:21:01 -0700

    Removing unused function

--- app/components/PostList.js
+++ app/components/PostList.js
@@ -16,42 +16,6 @@ const List = props => {
     props.retrievePosts();
   });
 
-  const getActivePosts = () => {
-    if (props.posts.length === 0 || props.tags.length === 0) {
-      return props.posts;
-    }
-
-    const tags = props.tags;
-    let posts = props.posts;
-    let activePosts = [];
-    let activeTags = [];
-    let tagsActive;
-
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].active) activeTags.push(tags[i]);
-    }
-
-    if (activeTags.length) {
-      for (let i = 0; i < posts.length; i++) {
-        tagsActive = 0;
-
-        for (let j = 0; j < activeTags.length; j++) {
-          for (let k = 0; k < posts[i].tag.length; k++) {
-            if (activeTags[j].id === posts[i].tag[k].id) {
-              tagsActive++;
-            }
-          }
-        }
-        if (tagsActive === activeTags.length) {
-          activePosts.push(posts[i]);
-        }
-      }
-    } else {
-      activePosts = props.posts;
-    }
-
-    return activePosts;
-  };
   if (props.posts[0] === false) {
     return (
       <section id="splash">
@@ -64,7 +28,7 @@ const List = props => {
     return (
       <section id="post-list">
         <TagMenu />
-        {getActivePosts().map((post, index) => {
+        {props.posts.map((post, index) => {
           return (
             <Link key={index} to={'/post/' + post.id} className="post-item">
               <img src={post.thumbUrl} />
