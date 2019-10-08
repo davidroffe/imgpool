39ad2d66081b235ce230a9ced4d47f3b36b7c393
David Roffe <davidroffe@gmail.com>
2019-10-08 11:28:06 -0700

    Post info bar & non-functional options menu in PostSingle component

--- app/components/PostSingle.js
+++ app/components/PostSingle.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import axios from 'axios';
@@ -7,8 +8,13 @@ import TagMenu from './TagMenu';
 const Single = props => {
   const [post, setPost] = useState({
     id: props.match.params.id || '',
-    tag: []
+    tag: [],
+    user: {
+      id: '',
+      username: ''
+    }
   });
+  const [optionsMenu, setOptionsMenu] = useState(false);
 
   useEffect(() => {
     axios
@@ -28,7 +34,31 @@ const Single = props => {
     <section id="post-single">
       <TagMenu tags={getTagsFromPosts(post)} />
       <div className="image-container">
-        <img src={post.url} />
+        <div className="inner">
+          <div className="post-info">
+            <button
+              className="toggle-options"
+              onClick={() => setOptionsMenu(!optionsMenu)}
+            >
+              options <span>+</span>
+            </button>
+            <div className={`options${optionsMenu ? ' active' : ''}`}>
+              <button className="toggle-fav">
+                <span className="icon">&hearts;</span>
+                <span className="text">add to favorites</span>
+              </button>
+              <button className="flag-post">
+                <span className="icon flag">&#9873;</span>
+                <span className="text">flag post</span>
+              </button>
+            </div>
+            <p className="poster">
+              posted by:{' '}
+              <Link to={`/user/${post.user.id}`}>{post.user.username}</Link>
+            </p>
+          </div>
+          <img src={post.url} />
+        </div>
       </div>
     </section>
   );
