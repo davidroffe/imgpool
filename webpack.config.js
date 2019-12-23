4cfd33d98f90ab35a5555526434350b83194d9eb
David Roffe <davidroffe@gmail.com>
2019-12-23 12:44:50 -0800

    Moving babel config from package.json to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,17 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      },
       {
         test: /\.less$/,
         use: [
